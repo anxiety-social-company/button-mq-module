0bd24d99f881c68cd895d7bde812519d5e09c332
louis.y <louis.y.2.tcg@gmail.com>
2025-04-13 16:12:43 +0800

    init: create new project

---
+++ src/test/java/com/asc/button/mq/module/ButtonMqModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asc.button.mq.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonMqModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
